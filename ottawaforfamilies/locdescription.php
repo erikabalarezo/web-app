91db7b96d8f3df0f92281a82c2760f6b61024e0f
erikabalarezo <erikabalarezo@yahoo.com>
2012-07-16 13:25:23 -0400

    Added changes to take only id.

--- ottawaforfamilies/locdescription.php
+++ ottawaforfamilies/locdescription.php
@@ -3,20 +3,19 @@
 require_once 'includes/db.php';
 require_once 'includes/functions.php';
 
-$name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
-$id = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_NUMBER_INT);
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-if (empty($name)) {
-header('Location: index.php');
-exit;
+if (empty($id)) {
+	header('Location: index.php');
+	exit;
 }
 
 $sql = $db->prepare('
 SELECT name, description
 FROM off_location
-WHERE name = :name
+WHERE id = :id
 ');
-$sql->bindValue(':name', $name, PDO::PARAM_STR);
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
 $sql->execute();
 $details= $sql->fetchALL();
 
