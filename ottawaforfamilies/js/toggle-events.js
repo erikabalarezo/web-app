55a8d6397abf7161f3610edefad465d35ab1733f
unknown <owner@owner-HP.(none)>
2012-08-07 22:29:31 -0400

    toggle-events.js created.

---
+++ ottawaforfamilies/js/toggle-events.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$(document).ready(function(){
+ 	//$('.item-name').click(function () {
+	//$('.toggle-icon').click(function () {
+		$loc['name'].click(function() {
+	$('.events').slideToggle("slow");
+	//$('.categorytab').click(function () {
+	//$('.item-name').slideToggle("slow");
+  });
+});
