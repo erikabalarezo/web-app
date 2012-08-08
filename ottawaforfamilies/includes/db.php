c921d8a40b96df2168cd989e052a7f129bd44479
erikabalarezo <erikabalarezo@yahoo.com>
2012-08-08 13:30:41 -0400

    Changed settings for phpfog.

--- ottawaforfamilies/includes/db.php
+++ ottawaforfamilies/includes/db.php
@@ -1,9 +1,13 @@
 <?php
+//Opens a dconnection to the MYSQL databse
+//Shared beteween all the pHP files in our application
 
-$dsn = getenv('DATA_SOURCE');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+$user = getenv('MYSQL_USERNAME');
+$pass = getenv('MYSQL_PASSWORD');
+$host = getenv('MYSQL_DB_HOST');//The MySQL username
+$dbname = getenv('MYSQL_DB_NAME');//The MySQL password
 
-
-$db = new PDO($dsn, $user, $pass);
+$data_source = sprintf('mysql:host=%s;dbname=%s', $host, $dbname);
+//PDO: PHP Data Object
+$db = new PDO($data_source, $user, $pass);
 $db->exec('SET NAMES utf8');
