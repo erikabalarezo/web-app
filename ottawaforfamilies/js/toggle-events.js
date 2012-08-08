4ccd74161f4ee2e427c5e9c3b14092d2e6b80bdd
erikabalarezo <erikabalarezo@yahoo.com>
2012-08-08 13:03:39 -0400

    Changed code. Works.

--- ottawaforfamilies/js/toggle-events.js
+++ ottawaforfamilies/js/toggle-events.js
@@ -2,13 +2,22 @@
 
 
 $(document).ready(function(){
+	$('.events').slideToggle("fast");
+	
+	$('.events').each(function () {
+		if ($(this).children('li').length <= 0) {
+			$('[data-location="' + $(this).data('location-events') + '"]').remove();
+		}
+	});
+	
  	//$('.item-name').click(function () {
 	//$('.toggle-icon').click(function () {
-		var location = $(document.getElementById('toggle').className);
-		//location.click(function() {
-		$('#toggle').click(function () {
 		
-	$('.events').slideToggle("slow");
+		//location.click(function() {
+		$('.toggle').click(function () {
+			var eventsList = '[data-location-events="' + $(this).data('location') + '"]';
+			$(eventsList).slideToggle('slow');
+	
 	//$('.categorytab').click(function () {
 	//$('.item-name').slideToggle("slow");
   });
